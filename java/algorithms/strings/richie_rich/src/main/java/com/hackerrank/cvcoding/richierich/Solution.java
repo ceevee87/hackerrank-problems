079a76aa2e839a7b37009f74b202964a59491b53
John Ramirez <cyclopsvision87@icloud.com>
2017-05-04 11:10:03 -0600

    add in comments to code. tighten up some corner case checks. refactor some code (remove giant else statement). add in procedure to check if a string is a palindrome.

--- java/algorithms/strings/richie_rich/src/main/java/com/hackerrank/cvcoding/richierich/Solution.java
+++ java/algorithms/strings/richie_rich/src/main/java/com/hackerrank/cvcoding/richierich/Solution.java
@@ -14,6 +14,21 @@ import java.util.Scanner;
  */
 public class Solution {
     
+    private static boolean isPalindrome(String str) {
+        if (str.length() < 2) return true;
+        
+        int ii = 0;
+        int jj = str.length() - 1;
+        
+        while (ii < jj) {
+          if (str.charAt(ii) != str.charAt(jj))
+              return false;
+          ii++;
+          jj--;
+        }
+        return true;
+    }
+
     public static int countCharOccurence(String s, char c) {
         long count = s.toLowerCase().chars().filter(e -> e == c).count();
         return (int) count;
@@ -40,11 +55,25 @@ public class Solution {
 
     public static String getMaxPalindromeKchanges(String number, int k) {
         int numLen = number.length();
+        
+        // what if we are allowed to make more changes than there are digits
+        // in the number? In that case, the answer will always be "99999...".
+        // let's do that here.
         if (k >= numLen) {
             return String.join("", Collections.nCopies(numLen, "9"));
         }
-        if (numLen == 1)
+        
+        // unless we are not allowed to change any digits then the largest
+        // single digit palindrome is 9.
+        if (numLen == 1 && k > 0)
             return "9";
+        
+        // this is the scenario where we are not allowed to change anything!
+        // boo!!!
+        if (k == 0) {
+            if (isPalindrome(number)) return number;
+            return "-1";
+        }
         
         // it matters if the number has an even or odd number of digits.
         int ii, jj;
@@ -56,6 +85,13 @@ public class Solution {
             ii = jj-1;
         }
         
+        // we are going to make a palindrome out of the input number.
+        // this is a base requirement of the problem before we attempt to 
+        // make it as large as possible.
+        // now, we keep a special count of the 9 digit because it is special
+        // later in the algorithm as we attempt to make the palindrome as large
+        // as possible we'll be specifically adding in 9s. So, we'll account
+        // for how many 9s we run into first while scanning the number-string.
         int da_nines = countCharOccurence(number, '9');
         while (jj < numLen) {
             if (k >= (2*(ii+1)-da_nines))
@@ -82,42 +118,50 @@ public class Solution {
             ii--;
             jj++;
         }
-        
+
+        // we were barely able to convert the number to a palindrome in k moves.
+        // we're done here. we can't make any more changes.
         if (k == 0) {
             return number;
         }
         
+        // we were unable to convert the number to a palindrome in k moves.
+        // we're done here. we can't make any more changes. return failure code.
         if (k < 0)
           return "-1";
-        else {
-            // k > 0
-            // we have enough k-s to make this number a palindrome.
-            // we just need to fill in as many 9s as possible starting
-            // at the edges and working inwards.
-            if (k > 1) {
-                ii = 0;
-                while (ii < numLen/2 && k > 0) {
-                    if (number.charAt(ii) != '9') {
-                        number = replaceCharAt(number, ii, '9');
-                        k--;
-                    }
-                    if (number.charAt(numLen-ii-1) != '9') {
-                        number = replaceCharAt(number, numLen-ii-1, '9');
-                        k--;
-                    }
-                    ii++;
-                }
-            }                        
-            // if there is a left-over k then let's deal with it
-            // the case below is only for odd-length numbers
-            // and k should equal 1 at this point
-            if ((numLen % 2) > 0 && (k % 2) > 0) {
-                if (number.charAt((numLen-1)/2) != '9') {
-                    number = replaceCharAt(number, (numLen-1)/2, '9');
+
+        // k > 0
+        // we were able to convert the number to palindrome as we still have
+        // some extra moves (that amount is k) to make!!! 
+        // let's use the remaining moves to fill in as many 9s as possible 
+        // starting at the edges and working inwards.
+        if (k > 1) {
+            ii = 0;
+            // we don't need a left pointer counting up and a right pointer
+            // counting down. this is a palindrome. we'll just track the
+            // left pointer (array index). from that we can calculate the
+            // right point (array index).
+            while (ii < numLen/2 && k > 0) {
+                if (number.charAt(ii) != '9') {
+                    number = replaceCharAt(number, ii, '9');
                     k--;
                 }
+                if (number.charAt(numLen-ii-1) != '9') {
+                    number = replaceCharAt(number, numLen-ii-1, '9');
+                    k--;
+                }
+                ii++;
             }
-            return number;
+        }                        
+        // if there is a left-over k then let's deal with it
+        // the case below is only for odd-length numbers
+        // and k should equal 1 at this point
+        if ((numLen % 2) > 0 && (k % 2) > 0) {
+            if (number.charAt((numLen-1)/2) != '9') {
+                number = replaceCharAt(number, (numLen-1)/2, '9');
+                k--;
+            }
         }
+        return number;
     }
 }
